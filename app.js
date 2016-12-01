106d2483cdaca3a8e0a329863c1793c1af52d782
Ghaz-Siddique <ghaz_siddique@yahoo.com>
2016-12-01 11:49:27 +0500

    Adding Slider Code

---
+++ app.js
@@ -0,0 +1,25 @@
+var slides = document.querySelectorAll('#slides .slide');
+var currentSlide = 0;
+var slideInterval = setInterval(nextSlide,2000);
+
+function nextSlide(){
+	slides[currentSlide].className = 'slide';
+	currentSlide = (currentSlide+1)%slides.length;
+	slides[currentSlide].className = 'slide showing';
+}
+
+
+var myIndex = 0;
+slideShow();
+
+function slideShow() {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    for (i = 0; i < x.length; i++) {
+      x[i].style.display = "none";  
+    }
+    myIndex++;
+    if (myIndex > x.length) {myIndex = 1}    
+    x[myIndex-1].style.display = "block";  
+    setTimeout(slideShow, 2500);    
+}
